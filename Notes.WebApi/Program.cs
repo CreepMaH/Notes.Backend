7988c6f24da955a1deac4c0cd0926e978afc3560
Сергей Винчестер <creepmah@gmail.com>
2024-01-06 13:50:08 +0300

    Реализованы эндпоинты. Получена рабочая версия приложения.

--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,16 +1,53 @@
+using Notes.Application;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Persistance;
+using Notes.Persistence;
+using System.Reflection;
+
 namespace Notes.WebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
 
             var app = builder.Build();
+            ConfigurePipeline(app);
 
+            app.Run();
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddScoped<NotesDbContext>();
+            builder.Services.AddAutoMapper(config =>
+            {
+                config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+            });
+            builder.Services.AddApplication();
+            builder.Services.AddPersistence(builder.Configuration);
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyOrigin();
+                });
+            });
+
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            DbInitializer.Initialize(serviceProvider.GetRequiredService<NotesDbContext>());
+        }
+
+        private static void ConfigurePipeline(WebApplication app)
+        {
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
@@ -18,17 +55,18 @@ namespace Notes.WebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
